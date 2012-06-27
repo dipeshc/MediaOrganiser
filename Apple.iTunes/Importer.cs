799071fa55bcedb0f1b1aea6877e03e76e318771
Dipesh Chauhan <dipesh@chauhan.net.au>
2012-06-28 08:32:51 +1000

    Added Apple.iTunes project with UNFINISHED importer class and script

---
+++ Apple.iTunes/Importer.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Apple.iTunes
+{
+	public static class Importer
+	{
+		public static Boolean Add(IFile File)
+		{
+		}
+
+		public static Boolean Add(IEnumerable<IFile> Files)
+		{
+		}
+
+		public static Boolean Add(IDirectory Directory)
+		{
+		}
+
+		public static Boolean Add(IEnumerable<IDirectory> Directory)
+		{
+		}
+	}
+}
+
