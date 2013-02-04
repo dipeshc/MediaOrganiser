853c92e78188af262e5b4963c761c48982a68dfd
Dipesh Chauhan <dipesh@chauhan.net.au>
2013-02-04 20:59:25 +1100

    General maintenance. Renamed variables.

--- Src/MediaOrganiser.Console/Commands/Organise.cs
+++ Src/MediaOrganiser.Console/Commands/Organise.cs
@@ -14,42 +14,42 @@ namespace MediaOrganiser.Console
 	{
 		private static IFileSystem _fileSystem = new FileSystem();
 
-		string inputPath = "";
-		DirectoryInfoBase outputDirectory = null;
-		bool forceConversion = false;
-		bool exclude = false;
-		bool strictSeason = false;
+		private string _inputPath = "";
+		private DirectoryInfoBase _outputDirectory = null;
+		private bool _forceConversion = false;
+		private bool _exclude = false;
+		private bool _strictSeason = false;
 
 		public Organise()
 		{
 			IsCommand("organise", "Organises the media (TV Shows) into a uniform ouput which can be consumed.");
-			HasRequiredOption("i|input=", "The Input path from which media will be found.", v => inputPath = v);
-			HasRequiredOption("o|output=", "The Output directory from which organised media will be put.", v => outputDirectory = _fileSystem.DirectoryInfo.FromDirectoryName(v));
-			HasOption("f|forceConversion", "Forces the conversion of all input media, even if the media is already in the correct format.", v => forceConversion = v != null);
-			HasOption("x|exclude", "Excludes organised shows in output directory when searching the input path.", v => exclude = v != null);
-			HasOption("s|strictSeason", "Enforces a strict season number requirement. This will guarantee the output media contains season details.", v => strictSeason = v != null);
+			HasRequiredOption("i|input=", "The Input path from which media will be found.", v => _inputPath = v);
+			HasRequiredOption("o|output=", "The Output directory from which organised media will be put.", v => _outputDirectory = _fileSystem.DirectoryInfo.FromDirectoryName(v));
+			HasOption("f|forceConversion", "Forces the conversion of all input media, even if the media is already in the correct format.", v => _forceConversion = v != null);
+			HasOption("x|exclude", "Excludes organised shows in output directory when searching the input path.", v => _exclude = v != null);
+			HasOption("s|strictSeason", "Enforces a strict season number requirement. This will guarantee the output media contains season details.", v => _strictSeason = v != null);
 		}
 
 		public override int Run(string[] remainingArguments)
 		{
 			// Check inputPath exists.
-			if(!_fileSystem.DirectoryInfo.FromDirectoryName(inputPath).Exists && !_fileSystem.FileInfo.FromFileName(inputPath).Exists)
+			if(!_fileSystem.DirectoryInfo.FromDirectoryName(_inputPath).Exists && !_fileSystem.FileInfo.FromFileName(_inputPath).Exists)
 			{
 				throw new ApplicationException("Invalid input path provided.");
 			}
 			
 			// Check output folder exists.
-			if(!outputDirectory.Exists)
+			if(!_outputDirectory.Exists)
 			{
 				throw new ApplicationException("Invalid ouput directory provided.");
 			}
 			
 			// Setup paths.
-			var inputPaths = new List<string>() { inputPath };
+			var inputPaths = new List<string>() { _inputPath };
 			var excludedPaths = new List<string>();
-			if(exclude)
+			if(_exclude)
 			{
-				excludedPaths.Add(outputDirectory.FullName);
+				excludedPaths.Add(_outputDirectory.FullName);
 			}
 
 			// Find shows and movies.
@@ -70,7 +70,7 @@ namespace MediaOrganiser.Console
 			var organiser = new Organiser();
 			mediaToOrganise.ForEach(media =>
 			{
-				organiser.Organise(media, outputDirectory, forceConversion, strictSeason);
+				organiser.Organise(media, _outputDirectory, _forceConversion, _strictSeason);
 			});
 
 			// Return 0.
